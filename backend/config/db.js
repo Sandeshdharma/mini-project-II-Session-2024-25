e4cf8da78f3124029e1a0536e1567e5ca4f4cd82
Sandesh Raj <sandeshsri01@gmail.com>
2025-04-15 02:10:33 +0530

    NEW DB:MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const { URI, DB_NAME } = require("../constants");
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`mongodb://localhost:27017/`);
+    const connectionInstance = await mongoose.connect(`${URI}/${DB_NAME}`);
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
